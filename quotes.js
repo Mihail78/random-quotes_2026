43da2d9d1efdb1583161cc0bfb578758dd603d03
Mihai Temciuc <temciucm@gmail.com>
2026-03-31 19:30:47 +0300

    Splitting app into the JS modules

---
+++ quotes.js
@@ -0,0 +1,44 @@
+const quotes = [
+    {
+        quote: 'Be yourself; everyone else is already taken.',
+        author: 'Oscar Wilde',
+    },
+    {
+        quote: 'In the middle of every difficulty lies opportunity.',
+        author: 'Albert Einstein',
+    },
+    {
+        quote: 'The journey of a thousand miles begins with one step.',
+        author: 'Lao Tzu',
+    },
+    {
+        quote: 'That which does not kill us makes us stronger.',
+        author: 'Friedrich Nietzsche',
+    },
+    {
+        quote: 'Life is what happens when you’re busy making other plans.',
+        author: 'John Lennon',
+    },
+    {
+        quote: 'Do what you can, with what you have, where you are.',
+        author: 'Theodore Roosevelt',
+    },
+    {
+        quote: 'Success usually comes to those who are too busy to be looking for it.',
+        author: 'Henry David Thoreau',
+    },
+    {
+        quote: 'Happiness depends upon ourselves.',
+        author: 'Aristotle',
+    },
+    {
+        quote: 'Turn your wounds into wisdom.',
+        author: 'Oprah Winfrey',
+    },
+    {
+        quote: 'The purpose of our lives is to be happy.',
+        author: 'Dalai Lama',
+    },
+];
+
+export default quotes;
